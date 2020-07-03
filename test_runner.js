d3303bc38f9332ac7b953a0240b6e88f068d2561
Kynan Stewart <2020@mx.kynan.net.au>
2020-07-03 16:30:05 +1000

    Initial commit

---
+++ test_runner.js
@@ -0,0 +1,10 @@
+function pass(message) {
+  console.log(message);
+}
+function fail(message) {
+  console.error(message);
+}
+function Test(message, testFunction) {
+  console.log(message);
+  testFunction();
+}
