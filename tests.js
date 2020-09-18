541d52998b228516309043f6e475239a8b9afabb
Dean Tedesco <dino@tedesco.net.au>
2020-09-18 16:15:38 +1000

    WIP - Adding test 1 moveElements function

--- tests.js
+++ tests.js
@@ -1,6 +1,11 @@
 // @flow
 Test("First test", () => {
-  const result /*: boolean */ = exampleFunction();
+  const inputArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  const outputArray = ["A", "E", "F", "G", "H", "B", "C", "D", "I", "J"];
+  const startIndex = 1;
+  const endIndex = 3;
+  const beforeIndex = 8;
+  const result /*: boolean */ = moveElements(inputArray, startIndex, endIndex, beforeIndex);
 
   if (result === true) {
     pass(`The result is ${result.toString()}`);
